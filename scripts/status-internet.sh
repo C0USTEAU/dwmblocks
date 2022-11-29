3011a9772cfdb6bd4c0c081166c093f8b8e1972e
C0USTEAU <egan.feargal.web@gmail.com>
2022-11-29 22:37:30 +0000

    Updated internet emoji for block

--- scripts/status-internet.sh
+++ scripts/status-internet.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-cat /proc/net/wireless  | awk '$1 ~ /wlan0:/ {printf "%4s%3.0f%%%2s", "  ℹ️ ", $3 * 100 / 70, "  " }' &&   iwconfig wlan0 | grep wlan0 | sed 's/.*SSID:\"\(.*\)\".*/\1  /'
+cat /proc/net/wireless  | awk '$1 ~ /wlan0:/ {printf "%4s%3.0f%%%2s", "  📶 ", $3 * 100 / 70, "  " }' &&   iwconfig wlan0 | grep wlan0 | sed 's/.*SSID:\"\(.*\)\".*/\1  /'
