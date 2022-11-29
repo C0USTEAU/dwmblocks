ac68c6b69d72f5a943ceebfef2682c16909d7e1e
C0USTEAU <egan.feargal.web@gmail.com>
2022-11-29 14:27:25 +0000

    Updated the signal numbers for blocks, initial numbers were too high as I'm a dumbass

--- blocks.h
+++ blocks.h
@@ -1,14 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "status-pacman.sh", 0, 30},
-	{"", "status-brightness.sh", 0, 40},
-	{"", "status-cpu-temp.sh", 60, 50},
-	{"", "status-cpu.sh", 5, 60},
-	{"", "status-memory.sh", 5, 70},
-	{"", "status-internet.sh", 10, 80},
-	{"", "status-battery.sh", 2, 90},
-	{"", "status-clock.sh", 1, 100}
+	{"", "status-pacman.sh", 0, 8},
+	{"", "status-brightness.sh", 0, 7},
+	{"", "status-cpu-temp.sh", 60, 6},
+	{"", "status-cpu.sh", 5, 5},
+	{"", "status-memory.sh", 5, 4},
+	{"", "status-internet.sh", 10, 3},
+	{"", "status-battery.sh", 60, 2},
+	{"", "status-clock.sh", 1, 1}
 };
 
 
