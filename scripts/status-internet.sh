f153441fcc11df1b0521b45db51ddc5d3501452a
Feargal Egan <egan.feargal.web@gmail.com>
2023-05-02 19:58:47 +0000

    Updated status-internet.sh script to work for any wireless interface that starts with wl - not  just wlan0

--- scripts/status-internet.sh
+++ scripts/status-internet.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-cat /proc/net/wireless  | awk '$1 ~ /wlan0:/ {printf "%4s%3.0f%%%2s", "  📶 ", $3 * 100 / 70, "  " }' &&   iwconfig wlan0 | grep wlan0 | sed 's/.*SSID:\"\(.*\)\".*/\1  /'
+cat /proc/net/wireless  | awk '$1 ~ /wl/ {printf "%4s%3.0f%%%2s", "  📶 ", $3 * 100 / 70, "  " }' &&   iwconfig 2>/dev/null | grep wl | sed 's/.*SSID:\"\(.*\)\".*/\1  /'
